c1840adacfec68b3f6416dafdde75e2555ecd418
shailendr pawar <shailendrapawar980@gmail.com>
2025-01-27 12:45:53 +0530

    ADDED: fucntionality for cart number count

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,7 @@ export default function({data}){
 return (
     <div className="card-body bg-white rounded-md " >
         <img className="h-7/10 w-full object-contain p-1" src={data.image}></img>
-        <p className=" text-black h-6 overflow-hidden text-ellipsis break-words ">{data.title}</p>
+        <p className=" text-black h-6 overflow-hidden text-ellipsis break-words text- ">{data.title}</p>
         <div className=" flex justify-between pr-4 pl-4 h-8">
             <button className="bg-red-500 w-16 rounded-md" onClick={()=>{
                 dispatch({type:"wishlist/add",payload:data})
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,29 @@
 import "./navbar.css"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link, NavLink } from "react-router-dom"
 import { FaShoppingCart } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 
 export default function Navbar() {
+    const cartitems=useSelector((state)=>state.cart)
+    const [count,setCount]=useState(0)
+
+     useEffect(()=>{
+
+        const c=cartitems.reduce((acc,curr)=>{
+            return  acc+=curr.qty
+        },0)
+        setCount(c)
+
+     },[cartitems])
     return (
         <nav className="navbar flex justify-center gap-10 items-center">
             <div className="w-2/4 max-w-52 flex justify-between">
                 <NavLink className="w-16  rounded-md" to="/">Home</NavLink>
                 <NavLink className="w-16 rounded-md" to="/wishlist">wishlist</NavLink>
             </div>
-            <NavLink className="w-9 h-8 rounded-md flex justify-center items-center" to="/cart"><FaShoppingCart /></NavLink>
+            <NavLink className="w-12 h-8 rounded-md flex justify-evenly items-center" to="/cart"><FaShoppingCart />{count}</NavLink>
         </nav>
     )
 
